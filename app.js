9251dc0514fc50709fa550297a530ae64ca8c88a
KaedenOC <komeacarr2@gmail.com>
2023-03-13 13:15:04 -0700

    add html to index and add five locations as objects

--- app.js
+++ app.js
@@ -2,3 +2,47 @@
 
 // store min/max hourly cust and avg cookies per customer in objects
 
+let location1 = {
+    name: 'Seattle',
+    minCust: 23,
+    maxCust: 65,
+    avgCookieSale: 6.3,
+}
+
+console.log(location1);
+
+let location2 = {
+    name: 'Tokyo',
+    minCust: 3,
+    maxCust: 24,
+    avgCookieSale: 1.2,
+}
+
+console.log(location2);
+
+let location3 = {
+    name: 'Dubai',
+    minCust: 11,
+    maxCust: 38,
+    avgCookieSale: 3.7,
+}
+
+console.log(location3);
+
+let location4 = {
+    name: 'Paris',
+    minCust: 20,
+    maxCust: 38,
+    avgCookieSale: 2.3,
+}
+
+console.log(location4);
+
+let location5 = {
+    name: 'Lima',
+    minCust: 2,
+    maxCust: 16,
+    avgCookieSale: 4.6,
+}
+
+console.log(location5);
